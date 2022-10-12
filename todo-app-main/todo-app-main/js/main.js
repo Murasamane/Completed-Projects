5f816037144114a56e3fed3292d89e128384c3a0
Giorgi Gurgenindze <giorgigurgenidze133@gmail.com>
2022-10-12 13:02:57 +0400

    -revamped whole JS -reformed adding and removing functions -revamped marking function -added active states for categories -added clear all function

--- todo-app-main/todo-app-main/js/main.js
+++ todo-app-main/todo-app-main/js/main.js
@@ -1,23 +1,66 @@
-const checks = document.querySelectorAll(".checkbox");
-console.log(checks);
-const checked = Array.from(checks);
-checked.forEach((item, index) => {
-  item.addEventListener("click", (e) => {
-    e.target.classList.toggle("checked");
-    e.target.parentNode.nextElementSibling.classList.toggle("checked-item");
-    // console.log(document.querySelectorAll(".checked-item"));
-  });
+"use strict";
+const form = document.querySelector(".input-todo");
+const outputUl = document.querySelector(".outputs");
+const clear = document.querySelector('.todo-clear-completed-btn');
+const todoCategories = document.querySelector('.todo-categories');
+const todoBtns = document.querySelectorAll('.todo-btn')
+document.querySelector(".item-count").textContent = document.querySelectorAll(".output-list-item").length;
+
+const template = (todo) => {
+  const html = `
+  <li class="output-list-item">
+  <label class="container">
+    <input type="checkbox" class="checkbox"name="checkedInput">
+    <span class="checkmark"></span>
+  </label>
+  <p class="list-item">${todo}</p>
+  <button class="delete">
+    <img src="images/icon-cross.svg"  class="delete-img" alt="">
+  </button>
+</li>
+  `;
+  outputUl.insertAdjacentHTML('afterbegin',html);
+  document.querySelector(".item-count").textContent = document.querySelectorAll(".output-list-item").length;
+};
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const todoVal = form.todo.value.trim();
+  if (todoVal.length) {
+    template(todoVal);
+    form.reset();
+  }
 });
-document.querySelector(".item-count").textContent = checked.length;
-document.querySelectorAll('.delete').forEach((itemDel)=>{
-    itemDel.addEventListener('click',(e)=>{
-        e.target.parentElement.parentNode.remove()
-    })
+
+outputUl.addEventListener("click", (e) => {
+  if (e.target.classList.contains("delete-img")) {
+    e.target.parentNode.parentElement.remove();
+  }
+  document.querySelector(".item-count").textContent = document.querySelectorAll(".output-list-item").length;
+
+  if (e.target.classList.contains("checkmark")) {
+    e.target.parentNode.nextElementSibling.classList.toggle("checked-item");
+  }
+});
+
+
+clear.addEventListener('click',e=>{
+  const marked =document.querySelectorAll('.checked-item')
+  marked.forEach(item=>{
+    item.parentNode.remove()
+  })
+  document.querySelector(".item-count").textContent = document.querySelectorAll(".output-list-item").length;
 })
 
+todoCategories.addEventListener('click',e=>{
+  const currentEl=e.target.classList;
 
-document.querySelector('.todo-clear-completed-btn').addEventListener('click',()=>{
-    document.querySelectorAll('.checked-item').forEach((e)=>{
-        e.parentElement.parentNode.remove();
+  if(!currentEl.contains('active')){
+    todoBtns.forEach((button)=>{
+      button.classList.remove("active")
     })
+  }
+
+  if(currentEl.contains("todo-btn")){
+    currentEl.add("active")
+    }
 })
