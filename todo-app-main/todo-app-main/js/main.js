bd7fbfbfb66de31abebf96a18b42f7441f880d64
Giorgi Gurgenindze <giorgigurgenidze133@gmail.com>
2022-10-12 15:46:38 +0400

    - added categorie changes - fixed known bugs - next up:  - gonna add dark and light mods  - gonna fix some styles and structure of HTML

--- todo-app-main/todo-app-main/js/main.js
+++ todo-app-main/todo-app-main/js/main.js
@@ -1,10 +1,11 @@
 "use strict";
 const form = document.querySelector(".input-todo");
 const outputUl = document.querySelector(".outputs");
-const clear = document.querySelector('.todo-clear-completed-btn');
-const todoCategories = document.querySelector('.todo-categories');
-const todoBtns = document.querySelectorAll('.todo-btn')
-document.querySelector(".item-count").textContent = document.querySelectorAll(".output-list-item").length;
+const clear = document.querySelector(".todo-clear-completed-btn");
+const todoCategories = document.querySelector(".todo-categories");
+const todoBtns = document.querySelectorAll(".todo-btn");
+document.querySelector(".item-count").textContent =
+  document.querySelectorAll(".output-list-item").length;
 
 const template = (todo) => {
   const html = `
@@ -19,8 +20,9 @@ const template = (todo) => {
   </button>
 </li>
   `;
-  outputUl.insertAdjacentHTML('afterbegin',html);
-  document.querySelector(".item-count").textContent = document.querySelectorAll(".output-list-item").length;
+  outputUl.insertAdjacentHTML("afterbegin", html);
+  document.querySelector(".item-count").textContent =
+    document.querySelectorAll(".output-list-item").length;
 };
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -35,32 +37,88 @@ outputUl.addEventListener("click", (e) => {
   if (e.target.classList.contains("delete-img")) {
     e.target.parentNode.parentElement.remove();
   }
-  document.querySelector(".item-count").textContent = document.querySelectorAll(".output-list-item").length;
+  document.querySelector(".item-count").textContent =
+    document.querySelectorAll(".output-list-item").length;
 
   if (e.target.classList.contains("checkmark")) {
     e.target.parentNode.nextElementSibling.classList.toggle("checked-item");
+    e.target.parentNode.parentElement.classList.toggle("completed");
+  }
+});
+
+clear.addEventListener("click", (e) => {
+  const marked = document.querySelectorAll(".checked-item");
+  marked.forEach((item) => {
+    item.parentNode.remove();
+  });
+  document.querySelector(".item-count").textContent =
+    document.querySelectorAll(".output-list-item").length;
+});
+
+todoCategories.addEventListener("click", (e) => {
+  const currentEl = e.target.classList;
+
+  if (!currentEl.contains("active")) {
+    todoBtns.forEach((button) => {
+      button.classList.remove("active");
+    });
+  }
+
+  if (currentEl.contains("todo-btn")) {
+    currentEl.add("active");
   }
 });
 
 
-clear.addEventListener('click',e=>{
-  const marked =document.querySelectorAll('.checked-item')
-  marked.forEach(item=>{
-    item.parentNode.remove()
-  })
-  document.querySelector(".item-count").textContent = document.querySelectorAll(".output-list-item").length;
-})
 
-todoCategories.addEventListener('click',e=>{
-  const currentEl=e.target.classList;
-
-  if(!currentEl.contains('active')){
-    todoBtns.forEach((button)=>{
-      button.classList.remove("active")
-    })
-  }
-
-  if(currentEl.contains("todo-btn")){
-    currentEl.add("active")
+const filteringCompleted = () => {
+  const arr = Array.from(outputUl.children);
+  const completedArr = arr.filter((item) => {
+    return item;
+  });
+  completedArr.forEach((item) => {
+    if (item.classList.contains("completed")) {
+      item.classList.remove("filtered-todo");
     }
-})
+    if (!item.classList.contains("completed")) {
+      item.classList.add("filtered-todo");
+    }
+  });
+};
+
+const filteringActive = () => {
+  const arr = Array.from(outputUl.children);
+  const activeArr = arr.filter((item) => {
+    return item;
+  });
+  activeArr.forEach((item) => {
+    if (item.classList.contains("filtered-todo")) {
+      item.classList.remove("filtered-todo");
+    }
+    if (item.classList.contains("completed")) {
+      item.classList.add("filtered-todo");
+    }
+  });
+};
+const filteringAll = () => {
+  const arr = Array.from(outputUl.children);
+  arr.forEach((item) => {
+    if (
+      item.classList.contains("filtered-todo") ||
+      !item.classList.contains("filtered-todo")
+    ) {
+      item.classList.remove("filtered-todo");
+    }
+  });
+};
+document.querySelector(".todo-completed").addEventListener("click", (e) => {
+  filteringCompleted();
+});
+
+document.querySelector(".todo-active").addEventListener("click", (e) => {
+  filteringActive();
+});
+
+document.querySelector(".todo-all").addEventListener("click", (e) => {
+  filteringAll();
+});
